2d5767cc7d68c613663cbf932ed8610dcb8f8131
luizamedeiros <65048102+luizamedeiros@users.noreply.github.com>
2021-09-05 19:10:08 -0300

    Final project

---
+++ src/Components/Cabecalho/index.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import bank_logo from "../../assets/images/bank_logo.svg";
+import styled from "styled-components";
+import { corPrimaria } from "../UI/variaveis";
+
+const StyledHeader = styled.nav`
+  background-color: ${corPrimaria};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  height: 50px;
+  width: 50px
+`;
+
+const BtnCabecalho = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  background: ${(props)=>(
+    //ternario. verifica se tiver props.primary, será passado white. caso contrário, será cor primaria.
+    props.primary ? "white" : corPrimaria
+  )};
+  color: ${(props)=>(
+    props.primary ? corPrimaria : "white"
+  )}
+`;
+
+const Cabecalho = () => {
+  return (
+    <StyledHeader>
+      <Logo src={bank_logo} alt="Logo Smart Bank" />
+      <div>
+        {/* Passando primary aqui para o botão saber o que é */}
+        <BtnCabecalho primary href="https://google.com">
+          Ajuda
+        </BtnCabecalho>
+        <BtnCabecalho href="https://google.com">
+          Sair
+        </BtnCabecalho>
+      </div>
+    </StyledHeader>
+  );
+};
+
+export default Cabecalho;
